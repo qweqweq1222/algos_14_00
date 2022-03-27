7c68a8d7fcc47f2689976f9f55ccad766637cd78
Andrey Dron <quankyyam@gmail.com>
2022-03-27 11:10:06 +0300

    prbly fixed

--- sort/main.cpp
+++ sort/main.cpp
@@ -4,6 +4,74 @@
 #include "sort.h"
 #include "doctest.h"
 
+void templates::compareSort(void** ppArray, int length, CompareSortType* pCompareFunc)
+{
+	void *buffer;
+	for(int i = 0; i < length - 1; ++i)
+	{
+		for (int j = 0; j < length - i -1; ++j)
+			if (pCompareFunc(ppArray[j], ppArray[j + 1]) == -1)
+			{
+
+				buffer = ppArray[j];
+				ppArray[j] = ppArray[j+1];
+				ppArray[j+1] = buffer;
+			}
+	}
+}
+
+void templates::mergeSortCardinal(void **ppArray, void **buffer, int length, CompareSortType pCompareFunc)
+{
+	// добавить функцию выделения памяти одним большим куском
+	if(length > 1)
+	{
+		int left_size = length / 2;
+		int right_size = length - left_size;
+
+		mergeSortCardinal(ppArray,buffer, left_size, pCompareFunc);
+		mergeSortCardinal(ppArray + left_size,buffer + left_size, right_size, pCompareFunc);
+
+		int lidx = 0;
+		int ridx = left_size;
+		int idx = 0;
+
+		while(lidx < left_size || ridx < length)
+		{
+
+			if (pCompareFunc(ppArray[lidx], ppArray[ridx]) == -1)
+			{
+				buffer[idx++] = ppArray[lidx];
+				lidx++;
+			}
+			else
+			{
+				buffer[idx++] = ppArray[ridx];
+				ridx++;
+			}
+			if (lidx == left_size)
+			{
+				for (int i = ridx; i < length; ++i)
+					buffer[idx++] = ppArray[i];
+				break;
+			}
+			if (ridx == length)
+			{
+				for (int i = lidx; i < left_size; ++i)
+					buffer[idx++] = ppArray[i];
+				break;
+			}
+		}
+		for(int i = 0; i < length; ++i)
+			ppArray[i] = buffer[i];
+	}
+}
+
+void templates::mergeSort(void **ppArray, int length, CompareSortType pCompareFunc)
+{
+	// добавить функцию выделения памяти одним большим куском
+	void **buffer = new void *[length];
+	mergeSortCardinal(ppArray, buffer, length, pCompareFunc);
+}
 struct my_struct {
 	std::string name;
 	int data;
@@ -36,10 +104,17 @@ my_struct** generate_data(const int N)
 		mass[i] = new my_struct{ GenerateString(i),rand()% 100};
 	return mass;
 }
+void Print(my_struct** mass, const int counter)
+{
+	for(int i = 0; i < counter; ++i)
+		std::cout << "name: " << mass[i]->name << " " << "data: " << mass[i]->data << std::endl;
+	std::cout << "_______________________________________________________________________________________" << std::endl;
+}
 
 TEST_CASE("checker")
 {
 	my_struct** mass = generate_data(counter);
+	Print(mass,counter);
 	templates::mergeSort(mass, counter, Compare);
 	bool check_;
 	for(int i = 0; i < counter - 1; ++i)
@@ -47,4 +122,5 @@ TEST_CASE("checker")
 		check_ = Compare(mass[i], mass[i+1]) == -1 || Compare(mass[i], mass[i+1]) == 0;
 		CHECK(check_ == true);
 	}
+	Print(mass,counter);
 }
--- sort/sort.h
+++ sort/sort.h
@@ -10,67 +10,10 @@ namespace templates
 	\li < 0 - pElem1 больше pElem2
 	*/
 	typedef  int (CompareSortType)(const void *pElem1, const void *pElem2);
-	void compareSort(void **ppArray, int length, CompareSortType pCompareFunc)
-	{
-		void *buffer;
-		for(int i = 0; i < length - 1; ++i)
-		{
-			for (int j = 0; j < length - i -1; ++j)
-				if (pCompareFunc(ppArray[j], ppArray[j + 1]) == -1)
-				{
-
-					buffer = ppArray[j];
-					ppArray[j] = ppArray[j+1];
-					ppArray[j+1] = buffer;
-				}
-		}
-	}
-	void mergeSort(void **ppArray, int length, CompareSortType pCompareFunc)
-	{
-		if(length > 1)
-		{
-			int left_size = length / 2;
-			int right_size = length - left_size;
-
-			mergeSort(ppArray,left_size, pCompareFunc);
-			mergeSort(ppArray + left_size, right_size, pCompareFunc);
-
-			int lidx = 0;
-			int ridx = left_size;
-			int idx = 0;
-			void* tmp_array[length];
-
-			while(lidx < left_size || ridx < length)
-			{
-
-					if (pCompareFunc(ppArray[lidx], ppArray[ridx]) == -1)
-					{
-						tmp_array[idx++] = ppArray[lidx];
-						lidx++;
-					}
-					else
-					{
-						tmp_array[idx++] = ppArray[ridx];
-						ridx++;
-					}
-					if (lidx == left_size)
-					{
-						for (int i = ridx; i < length; ++i)
-							tmp_array[idx++] = ppArray[i];
-						break;
-					}
-					if (ridx == length)
-					{
-						for (int i = lidx; i < left_size; ++i)
-							tmp_array[idx++] = ppArray[i];
-						break;
-					}
-			}
-			for(int i = 0; i < length; ++i)
-				ppArray[i] = tmp_array[i];
-		}
-	}
+	void mergeSort(void **ppArray, int length, CompareSortType pCompareFunc);
+	void mergeSortCardinal(void **ppArray, void **buffer, int length, CompareSortType pCompareFunc);
 	void heapSort(void **ppArray, int length, CompareSortType pCompareFunc);
+	void compareSort(void **ppArray, int length, CompareSortType pCompareFunc);
 
 	template <class T>
 	void compareSort(T **ppArray, int length, int (pCompareFunc)(const T *pElem1, const T *pElem2))
